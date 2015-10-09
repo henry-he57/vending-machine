6e445fec6b02bcd8a3ed6115b6888d462409ef9a
341167922 <341167922@109R030573ZHQ22.EDU.YRDSB.NET>
2015-10-09 11:57:39 -0400

    Merge origin/master
    Conflicts:
    	src/vendingmachine/Display.java

--- src/vendingmachine/Snack.java
+++ src/vendingmachine/Snack.java
@@ -75,8 +75,9 @@ public class Snack {
     /**
      * 
      * @param name Snack that has its qty being decreased by 1
+     * @param money used to check price of snack to money imputed
      */
-    public void snackPurchased(Snack name, int money){
+    public void snackPurchased(Snack name, double money){
         if (name.price <= money){
             name.qty --;
         }else {
